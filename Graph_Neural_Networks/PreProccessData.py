f7852132bee24f9448764d667eef8e8cd2d043a9
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2025-11-17 17:36:36 +0000

    got very basic pipeline implemented (doesn't work tho lmao). It has a multithreaded approach. What needs to be done now tho is to implement the converters for the loadtxt() function so that it can read in columns that dont have int/float vals.

--- Graph_Neural_Networks/PreProccessData.py
+++ Graph_Neural_Networks/PreProccessData.py
@@ -2,17 +2,38 @@ import numpy as np
 from torch_geometric.data import TensorAttr
 import torch
 import math
+from multiprocessing import Pool, cpu_count
 import os
-dir_path = "/home/diarmaid/Documents/VNF_Dataset"
 
-def importData():
+
+dir_path = "/home/diarmaid/Documents/VNF_Dataset" # TODO - Make this a dynamic import, maybe include the dataset in the project directory
+tensor_list = []
+ip_dict = {}
+
+
+def getFilePaths():
+    filepaths = []
     for i in [x for x in os.listdir(dir_path)]:
         current_path = os.path.join(dir_path, i, "v" + i, "csv")
         for file in os.listdir(current_path):
             csv_file = os.path.join(current_path, file)
-            with open(csv_file, "r") as f:
-               print(csv_file)
+            filepaths.append(csv_file)
+
+    return filepaths
+
+
+def processFile(filepath):
+    with open(filepath, "r") as f:
+        arr = np.loadtxt(f, delimiter=",",skiprows=1)
+        return torch.tensor(arr)
 
 
 if __name__ == '__main__':
-    importData()
+    paths = getFilePaths()
+
+    print(f"Found {len(paths)} CSV files.")
+
+    with Pool(cpu_count()) as pool:
+        tensor_list = pool.map(processFile, paths)
+
+    print(f"Loaded {len(tensor_list)} tensors.")
