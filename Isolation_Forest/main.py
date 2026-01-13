91dba7890221f5edf39d5c756420c3cdf0e86f18
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2026-01-13 14:34:26 +0000

    I have altered the code to read in the first session of every VNF service. This is because the first sessions only contain benign data. I have concatenated the data together into one training set. This now has a basic flow of an isolation forest run.

--- Isolation_Forest/main.py
+++ Isolation_Forest/main.py
@@ -1,33 +1,40 @@
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import LabelEncoder
 import VNFDatasetLoader
 
 if __name__ == "__main__":
     files = VNFDatasetLoader.getFilePaths()
-    dataset = pd.read_csv(files[0], header=0,low_memory=False,encoding="utf-8",on_bad_lines="skip")
+    training_files = []
+    for file in files:
+        file_path_array = file.split("/")
+        file_name_array = file_path_array[len(file_path_array)-1]
+        file_name_array = file_name_array.split("_")
+        session_number = file_name_array[1]
+        print(session_number)
+        if int(session_number) == 1:
+            training_files.append(file)
 
-    dataset = dataset.dropna(axis=1)
+    fullTrainingFrames = []
+    for file in training_files:
+        dataset = pd.read_csv(files[3], header=0,low_memory=False,encoding="utf-8",on_bad_lines="skip")
+        dataset.dropna(axis=0,how='all',inplace=True)
+        fullTrainingFrames.append(dataset)
 
-    datasetLabels = dataset.iloc[:,dataset.shape[1]-1].values
+    fullTrainingDataset = pd.concat(fullTrainingFrames)
+    fullTrainingDataset = fullTrainingDataset.dropna(axis=1)
 
-    dataset.drop("Label",axis=1,inplace=True)
+    datasetLabels = fullTrainingDataset.iloc[:,fullTrainingDataset.shape[1]-1].values
+    fullTrainingDataset.drop("Label",axis=1,inplace=True)
 
-    for col in dataset.select_dtypes(include=['object']).columns:
+    for col in fullTrainingDataset.select_dtypes(include=['object']).columns:
         label_encoder = LabelEncoder()
-        dataset[col] = label_encoder.fit_transform(dataset[col].astype(str))
+        fullTrainingDataset[col] = label_encoder.fit_transform(fullTrainingDataset[col].astype(str))
 
+    pd.set_option('display.max_columns', None)
+    print(fullTrainingDataset.head())
+    print(np.unique(datasetLabels,return_counts=True))
 
-    # df = pd.DataFrame(filled_in_dataset, columns=filled_in_dataset[0])
-    # categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
-
-
-    # df_pandas_encoded = pd.get_dummies(df, columns=['Src Country', 'Dst Country', 'Protocols', 'IP Protocol', 'Dst ASN', 'Version', 'URI', 'Host',  'Alt Name', 'GEO', ' Hostname'], drop_first=True)
-    #
-    # one_hot_encoded = encoder.fit_transform(df[categorical_columns])
-    #
-    # x_train = filled_in_dataset[1,0:40]
-    #
-    # isolation_forest = IsolationForest(n_estimators=100, max_samples=1000, random_state=0)
-    #
-    # isolation_forest.fit(x_train)
+    isolationForest = IsolationForest(n_estimators=100,random_state=0)
+    isolationForest.fit(fullTrainingDataset)
