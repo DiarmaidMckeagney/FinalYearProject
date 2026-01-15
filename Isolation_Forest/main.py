dc865c3a06b5aeb6b2765c0b503c8a13011d34fb
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2026-01-15 13:36:40 +0000

    Refactoring the code to separate out the code for getting the training files and importing the data into the VNFDatasetLoader. I found an issue with the Load_Balancer where one of the lines had no values other than a label, so I decided to remove that line.

--- Isolation_Forest/main.py
+++ Isolation_Forest/main.py
@@ -7,48 +7,27 @@ import VNFDatasetLoader
 
 if __name__ == "__main__":
     files = VNFDatasetLoader.getFilePaths()
-    training_files = []
-    for file in files:
-        file_path_array = file.split("/")
-        file_name_array = file_path_array[len(file_path_array)-1]
-        file_name_array = file_name_array.split("_")
-        session_number = file_name_array[1]
-        if int(session_number) == 1:
-            training_files.append(file)
+    training_files = VNFDatasetLoader.getFirstSessions(files)
 
-    fullTrainingFrames = []
-    for file in training_files:
-        dataset = pd.read_csv(files[3], header=0,low_memory=False,encoding="utf-8",on_bad_lines="skip")
-        dataset.dropna(axis=0,how='all',inplace=True)
-        fullTrainingFrames.append(dataset)
+    dataset, labels = VNFDatasetLoader.importDatasetFromFiles(training_files)
 
-    fullTrainingDataset = pd.concat(fullTrainingFrames)
-    fullTrainingDataset = fullTrainingDataset.dropna(axis=1)
-
-    datasetLabels = fullTrainingDataset.iloc[:,fullTrainingDataset.shape[1]-1].values
-    fullTrainingDataset.drop("Label",axis=1,inplace=True)
-
-    for col in fullTrainingDataset.select_dtypes(include=['object']).columns:
+    for col in dataset.select_dtypes(include=['object']).columns:
         label_encoder = LabelEncoder()
-        fullTrainingDataset[col] = label_encoder.fit_transform(fullTrainingDataset[col].astype(str))
+        dataset[col] = label_encoder.fit_transform(dataset[col].astype(str))
 
     isolationForest = IsolationForest(n_estimators=200, max_features=6, random_state=56)
-    isolationForest.fit(fullTrainingDataset)
-    isolationForest.fit(fullTrainingDataset)
+    isolationForest.fit(dataset)
 
-    testingdataset = pd.read_csv(files[2], header=0,low_memory=False,encoding="utf-8",on_bad_lines="skip")
-    testingdataset.dropna(axis=0,how='all',inplace=True)
-    testingdataset = testingdataset.dropna(axis=1)
-    testingLabels = testingdataset.iloc[:,testingdataset.shape[1]-1].values
-    testingdataset.drop("Label",axis=1,inplace=True)
+    testingDataset,testingLabels = VNFDatasetLoader.importDatasetFromFiles([files[2], files[3]])
 
-    for col in testingdataset.select_dtypes(include=['object']).columns:
+
+    for col in testingDataset.select_dtypes(include=['object']).columns:
         label_encoder = LabelEncoder()
-        testingdataset[col] = label_encoder.fit_transform(testingdataset[col].astype(str))
+        testingDataset[col] = label_encoder.fit_transform(testingDataset[col].astype(str))
 
     print(np.unique(testingLabels, return_counts=True))
 
-    predictions = isolationForest.predict(testingdataset)
+    predictions = isolationForest.predict(testingDataset)
 
     print(roc_auc_score(testingLabels, predictions))
     print("number of anomalies detected: ", list(predictions).count(-1))
--- VNFDatasetLoader.py
+++ VNFDatasetLoader.py
@@ -1,5 +1,6 @@
 
 import os
+import pandas as pd
 dir_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FinalYearProject/VNF_Dataset")# might be a better solution to this but it works
 
 def getFilePaths():
@@ -13,7 +14,29 @@ def getFilePaths():
     return filepaths
 
 
+def getFirstSessions(filepaths):
+    training_files = []
+    for file in filepaths:
+        file_path_array = file.split("/")
+        file_name_array = file_path_array[len(file_path_array) - 1]
+        file_name_array = file_name_array.split("_")
+        session_number = file_name_array[1]
+        if int(session_number) == 1:
+            training_files.append(file)
+    return training_files
 
 
+def importDatasetFromFiles(filepaths):
+    dataset = []
+    for file in filepaths:
+        datasetFrame = pd.read_csv(file, header=0, low_memory=False, encoding="utf-8", on_bad_lines="skip", skipinitialspace=True)
+        datasetFrame.dropna(axis=0, how='all', inplace=True)
+        dataset.append(datasetFrame)
 
+    fullDataset = pd.concat(dataset)
+    fullDataset = fullDataset.dropna(axis=1)
+    print(fullDataset.shape)
+    datasetLabels = fullDataset.iloc[:, fullDataset.shape[1] - 1].values
+    fullDataset.drop("Label", axis=1, inplace=True)
 
+    return fullDataset, datasetLabels
