5033e7ea37cb20978489e144e7982aef98f75f92
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2026-01-15 13:47:26 +0000

    Adding comments to make the flow of the code clear

--- Isolation_Forest/main.py
+++ Isolation_Forest/main.py
@@ -6,30 +6,32 @@ from sklearn.preprocessing import LabelEncoder
 import VNFDatasetLoader
 
 if __name__ == "__main__":
-    files = VNFDatasetLoader.getFilePaths()
-    training_files = VNFDatasetLoader.getFirstSessions(files)
+    files = VNFDatasetLoader.getFilePaths() # read in filepaths
+    training_files = VNFDatasetLoader.getFirstSessions(files) # get the first sessions to be used as the training data
 
-    dataset, labels = VNFDatasetLoader.importDatasetFromFiles(training_files)
+    dataset, labels = VNFDatasetLoader.importDatasetFromFiles(training_files) # import the dataset
 
-    for col in dataset.select_dtypes(include=['object']).columns:
+    for col in dataset.select_dtypes(include=['object']).columns: # use label encoding to encode any non-numeric column
         label_encoder = LabelEncoder()
         dataset[col] = label_encoder.fit_transform(dataset[col].astype(str))
 
-    isolationForest = IsolationForest(n_estimators=200, max_features=6, random_state=56)
-    isolationForest.fit(dataset)
+    isolationForest = IsolationForest(n_estimators=200, max_features=6, random_state=56) # create model
+    isolationForest.fit(dataset) # train model
 
-    testingDataset,testingLabels = VNFDatasetLoader.importDatasetFromFiles([files[2], files[3]])
+    testingDataset,testingLabels = VNFDatasetLoader.importDatasetFromFiles([files[2], files[3]]) # load some test data
 
 
-    for col in testingDataset.select_dtypes(include=['object']).columns:
+    for col in testingDataset.select_dtypes(include=['object']).columns: # perform label encoding on the testing data.
         label_encoder = LabelEncoder()
         testingDataset[col] = label_encoder.fit_transform(testingDataset[col].astype(str))
 
     print(np.unique(testingLabels, return_counts=True))
 
-    predictions = isolationForest.predict(testingDataset)
+    predictions = isolationForest.predict(testingDataset) # test the model
 
-    print(roc_auc_score(testingLabels, predictions))
+    print(roc_auc_score(testingLabels, predictions)) # print AUROC score
     print("number of anomalies detected: ", list(predictions).count(-1))
     print("number of 'Benign' predictions: ", list(predictions).count(1))
+
+
 
--- VNFDatasetLoader.py
+++ VNFDatasetLoader.py
@@ -4,6 +4,7 @@ import pandas as pd
 dir_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "FinalYearProject/VNF_Dataset")# might be a better solution to this but it works
 
 def getFilePaths():
+    # this function gets all the filepaths in the VNF_Dataset folder.
     filepaths = []
     for i in [x for x in os.listdir(dir_path)]:
         current_path = os.path.join(dir_path, i, "v" + i, "csv")
@@ -15,6 +16,7 @@ def getFilePaths():
 
 
 def getFirstSessions(filepaths):
+    #this function is used to get the filepaths to first session from each VNF service. I am doing this because the first sessions have only benign data.
     training_files = []
     for file in filepaths:
         file_path_array = file.split("/")
@@ -29,14 +31,14 @@ def getFirstSessions(filepaths):
 def importDatasetFromFiles(filepaths):
     dataset = []
     for file in filepaths:
-        datasetFrame = pd.read_csv(file, header=0, low_memory=False, encoding="utf-8", on_bad_lines="skip", skipinitialspace=True)
-        datasetFrame.dropna(axis=0, how='all', inplace=True)
-        dataset.append(datasetFrame)
+        datasetFrame = pd.read_csv(file, header=0, low_memory=False, encoding="utf-8", on_bad_lines="skip", skipinitialspace=True) #read in each file
+        datasetFrame.dropna(axis=0, how='all', inplace=True) # This drops any rows that have no values
+        dataset.append(datasetFrame) # add the dataframe to the array of all dataframes
 
-    fullDataset = pd.concat(dataset)
-    fullDataset = fullDataset.dropna(axis=1)
+    fullDataset = pd.concat(dataset) # this merges all the data from each file into one dataframe
+    fullDataset = fullDataset.dropna(axis=1)# this drops any column that contains null values. I am doing this to ensure only common columns are used in the model.
     print(fullDataset.shape)
-    datasetLabels = fullDataset.iloc[:, fullDataset.shape[1] - 1].values
-    fullDataset.drop("Label", axis=1, inplace=True)
+    datasetLabels = fullDataset.iloc[:, fullDataset.shape[1] - 1].values # seperate out the labels
+    fullDataset.drop("Label", axis=1, inplace=True) # drop the labels from the dataset
 
-    return fullDataset, datasetLabels
+    return fullDataset, datasetLabels # return dataset and labels
